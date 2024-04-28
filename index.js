2737c964c7f14fd23953d6c6aabd779b1b9382cb
Emrandu1989 <emrandu1989@gmail.com>
2024-04-28 16:58:58 +0600

    some bug fix in the server

--- index.js
+++ index.js
@@ -28,9 +28,9 @@ const client = new MongoClient(uri, {
 
 async function run() {
   try {
-    // Connect the client to the server	(optional starting in v4.7)
-    await client.connect();
-
+     
+    // await client.connect();
+    
     const craftCollection = client.db("Art&CraftDB").collection('art&Craft')
 
     app.get('/crafts', async (req, res) => {
